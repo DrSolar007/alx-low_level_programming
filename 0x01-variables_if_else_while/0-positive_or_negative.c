b3cd5b8e4cde855d941a7911f47b593a0b4fefd8
Solarin Sodiq <solarinboluwatife007@gmail.com>
2022-09-09 11:28:28 -0700

    posi

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,11 +6,12 @@
 * main - assigns random number to n
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	int n;
-	srand(time(0));
 	
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)

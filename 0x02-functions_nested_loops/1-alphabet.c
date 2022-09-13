c7013de110f6428fd8111e79a898bd795b0fe3e5
Solarin Sodiq <solarinboluwatife007@gmail.com>
2022-09-13 11:49:12 -0700

    alphac

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
 * Return: Always 0
 */
 
-int main(void)
+void print_alphabet(void)
 
 {
 
@@ -19,6 +19,6 @@ int main(void)
 						_putchar(c);
 
 				_putchar('\n');
-				return (0)
+				return (void)
 
 }

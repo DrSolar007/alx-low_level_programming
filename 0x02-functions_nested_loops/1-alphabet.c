54908608b9cabb713a5578e56dc308542baa663f
Solarin Sodiq <solarinboluwatife007@gmail.com>
2022-09-13 11:59:10 -0700

    alphac

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,25 @@
 #include "main.h"
 
+
+
 /**
-* print_alphabet - Entry point
-* Description: a function that prints the alphabet, in lowecase followed by a new line
-* Return: Always 0
+ *
+ *  * main - check the code for Holberton School students.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
 */
 
-void print_alphabet(void)
+int main(void)
 
 {
 	char c;
 	for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
-	_putchar('\n');
-	return (0)
+		_putchar(c)
+			_putchar(\n)
+
+			return (0);
+
 }

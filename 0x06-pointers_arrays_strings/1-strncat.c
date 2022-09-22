ae8a41f73872b539a29bdfc44d40ccc86bacef14
Solarin Sodiq <solarinboluwatife007@gmail.com>
2022-09-21 23:19:07 -0700

    strn

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -20,6 +20,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
+
 	j = 0;
 
 	while (j < n && src[j] != '\0')
@@ -28,7 +29,6 @@ char *_strncat(char *dest, char *src, int n)
 		j++;
 		i++;
 	}
-
 	dest[i] = '\0';
 	return (dest);
 }

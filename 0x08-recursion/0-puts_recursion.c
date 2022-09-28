215f0cb6efc334f424145970cb8d802e72604ae6
Solarin Sodiq <solarinboluwatife007@gmail.com>
2022-09-28 16:27:41 -0700

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**
+/*
 *_puts_recursion - prints a string
 *@s: pointer block of memory to fill
 *Return: void
@@ -15,6 +15,7 @@ void _puts_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
+
 	_putchar (*s);
 	_puts_recursion(s + 1);
 }

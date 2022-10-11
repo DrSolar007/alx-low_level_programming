8d9058855f94b10275e4defdf395517785ca16d8
Solarin Sodiq <solarinboluwatife007@gmail.com>
2022-10-10 18:42:04 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,33 +1,21 @@
 #ifndef FILE_DOG
-
 #define FILE_DOG
 
-
-
 /**
- *
- *  * struct dog - structure dog
- *
- *   * @name: atribute pointer to char name's dog
- *
- *    * @age: atribute float age's dog
- *
- *     * @owner: atribute pointer to char owner's dog
- *
- *      */
-
-
+* struct dog - structure dog
+* @name: atribute pointer to char name's dog
+* @age: atribute float age's dog
+* @owner: atribute pointer to char owner's dog
+*/
 
 struct dog
 
 {
+	char *name;
 
-		char *name;
+	float age;
 
-			float age;
-
-				char *owner;
-
+	char *owner;
 };
 
 
